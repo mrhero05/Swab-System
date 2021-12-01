7c5c6e84c55974c9abffd3cce6c8f47e10d75e91
humbledog30 <mikepialago@gmail.com>
2021-12-01 12:18:49 +0800

    12/01/21

---
+++ adminSide/adminDashboard.php
@@ -0,0 +1,207 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.admin.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <?php include '../layout/nav.admin.php';?>
+    <div class="container-fluid">
+        <div class="row">
+            <?php  include '../layout/sidebar.admin.php'; ?>
+            <div class="col-lg-10 p-4">
+                <div class="maincontainer">
+                    <h1>Dashboard</h1>
+                    <div class="row">
+                        <div class="col-md-12 col-xl-9">
+                            <div class="row">
+                                <div class="col-lg-12 dash-cards-container">
+                                    <div class="dash-cards" data-aos="flip-left">
+                                        <div class="dash-cards-img">
+                                            <img src="../image/recent.png" alt="">
+                                        </div>
+                                        <div class="dash-cards-info">
+                                            <span><span class="dash-cards-ctr">10</span>/45</span>
+                                            <span>Recent appointment</span>
+                                        </div>
+                                    </div>
+                                    <div class="dash-cards" data-aos="flip-left">
+                                        <div class="dash-cards-img">
+                                            <img src="../image/sched.png" alt="">
+                                        </div>
+                                        <div class="dash-cards-info">
+                                            <span><span class="dash-cards-ctr">8</span>/45</span>
+                                            <span>Scheduled for today</span>
+                                        </div>
+                                    </div>
+                                    <div class="dash-cards" data-aos="flip-left">
+                                        <div class="dash-cards-img">
+                                            <img src="../image/patient.png" alt="">
+                                        </div>
+                                        <div class="dash-cards-info">
+                                            <span class="dash-cards-ctr">222</span>
+                                            <span>Total patients</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 dash-upComing mt-5">
+                                    <div class="dash-upComing-header" >
+                                        <h4>Your upcoming patients</h4>
+                                        <div class="dash-upComing-btns">
+                                            <button class="btns-activee">Today</button>
+                                            <button>This week</button>
+                                            <button>This month</button>
+                                        </div>
+                                    </div>
+                                    <div class="dash-upComing-containter p-2" data-aos="fade-up">
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content" >
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                        <div class="dash-upComing-content">
+                                            <div class="dash-upComing-contentTop">
+                                                <span>9:30AM</span>
+                                                <span>12/21/20</span>
+                                            </div>
+                                            <div class="dash-upComing-contentBot">
+                                                <span>Sample Name</span>
+                                                <span>(252) 827-4674 Po Box 483 Macclesfield, North Carolina(NC), 27852</span>
+                                                <span>Status:Going</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 mt-md-5 m-auto">
+                            <div class="dash-recent">
+                                <h4>Recent Appointments</h4>
+                                <div class="dash-recent-table">
+                                    <table>
+                                        <thead>
+                                            <th>Name</th>
+                                            <th>Date</th>
+                                            <th>Time</th>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Sample Name</td>
+                                                <td>12/21/12</td>
+                                                <td>12:30AM</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Sample Name</td>
+                                                <td>12/21/12</td>
+                                                <td>12:30AM</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ adminSide/adminResults.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.admin.css">
+    <title>Results</title>
+</head>
+<body>
+    <?php include '../layout/nav.admin.php';?>
+    <div class="container-fluid">
+        <div class="row">
+            <?php  include '../layout/sidebar.admin.php'; ?>
+            <div class="col-lg-10 p-4">
+               <div class="resultContainer">
+                    <h1>Results</h1>
+                    <div class="result-table-container">
+                        <div class="result-table-filter">
+                            <input type="text" placeholder="Search...">
+                            <div class="result-table-filterRadio">
+                                <label><input type="radio" id="restableFilter" name="restableFilter" value="ID">ID</label>
+                                <label><input type="radio" id="restableFilter" name="restableFilter" value="Firstname">Firstname</label>
+                                <label><input type="radio" id="restableFilter" name="restableFilter" value="Lastname">Lastname</label>
+                                <button>Search</button>
+                            </div>
+                        </div>
+                        <div class="result-table-body">
+                            <table id="resultTableID" data-aos="fade-down">
+                                <thead>
+                                    <th>ID</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2DC213</td>
+                                        <td>Sample Name</td>
+                                        <td>Sample Name</td>
+                                        <td>Pending</td>
+                                        <td>
+                                            <button>Positive</button>
+                                            <button>Negative</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+               </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
